1cacb246058f350b997d6a67af4e8eb7318545bd
Engineertobe1 <engineertobe2010@gmail.com>
2023-12-23 16:49:30 -0500

    rtg

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// Get the parallax container and all the parallax images
-const parallaxContainer = document.querySelector('.parallax-container');
-const images = document.querySelectorAll('.image');
-
-// Function to handle scroll event
-function handleScroll() {
-  let scrollTop = window.scrollY;
-
-  // Loop through each image and apply parallax effect
-  images.forEach((image, index) => {
-    let speed = (index + 1) * 0.3; // Adjust speed as needed
-    let yOffset = -(scrollTop * speed);
-    image.style.transform = `translateY(${yOffset}px)`;
-  });
-}
-
-// Add scroll event listener
-window.addEventListener('scroll', handleScroll);
-
 // Hamburger menu functionality
 const menuToggle = document.querySelector('.menu-toggle');
 const navbar = document.querySelector('.navbar');
@@ -25,3 +6,28 @@ menuToggle.addEventListener('click', () => {
   navbar.classList.toggle('active');
   menuToggle.classList.toggle('active');
 });
+
+// Slideshow functionality
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlides() {
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[slideIndex].classList.add('active');
+  slideIndex = (slideIndex + 1) % slides.length;
+  setTimeout(showSlides, 3000); // Change slide every 3 seconds (adjust as needed)
+}
+
+showSlides();
+
+// Functions for slide navigation using arrows
+function plusSlides(n) {
+  slideIndex += n;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  } else if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[slideIndex].classList.add('active');
+}
