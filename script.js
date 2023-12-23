c2d0941a82df0c2a71710dcbb005b98605d0b9c7
Engineertobe1 <engineertobe2010@gmail.com>
2023-12-22 23:42:47 -0500

    ytdh

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+// Get the parallax container and all the parallax images
+const parallaxContainer = document.querySelector('.parallax-container');
+const images = document.querySelectorAll('.image');
+
+// Function to handle scroll event
+function handleScroll() {
+  let scrollTop = window.scrollY;
+
+  // Loop through each image and apply parallax effect
+  images.forEach((image, index) => {
+    let speed = (index + 1) * 0.3; // Adjust speed as needed
+    let yOffset = -(scrollTop * speed);
+    image.style.transform = `translateY(${yOffset}px)`;
+  });
+}
+
+// Add scroll event listener
+window.addEventListener('scroll', handleScroll);
+
+// Hamburger menu functionality
 const menuToggle = document.querySelector('.menu-toggle');
 const navbar = document.querySelector('.navbar');
 
 menuToggle.addEventListener('click', () => {
   navbar.classList.toggle('active');
+  menuToggle.classList.toggle('active');
 });
